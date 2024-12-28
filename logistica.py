dcd94c033206556cddab60b1536a8099faa88863
Matheus Gustavo <matheussgustavoo@gmail.com>
2024-12-28 12:03:34 -0300

    Primeiro commit

--- logistica.py
+++ logistica.py
@@ -177,7 +177,6 @@ search_space = {'penalty': ['l1', 'l2'], 'c': [100, 10, 1, 0.1, 0.01]}
 sampler = optuna.samplers.GridSampler(search_space=search_space)
 estudo_lr = optuna.create_study(directions=['maximize', 'maximize', 'minimize'])
 estudo_lr.optimize(lr_optuna, n_trials=10)
-
 best_trial = max(estudo_lr.best_trials, key=lambda t:t.values[1])
 
 print(f'Melhor trial:{best_trial.number}')
@@ -210,3 +209,14 @@ px.line(df_resultados_thresholds, x='cenario', y='resultado')
 import joblib
 
 joblib.dump(modelo_lr, 'fruts_model.pkl')
+
+data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada', 'Nevada'],
+'year': [2000, 2001, 2002, 2001, 2002, 2003],
+'pop': [1.5, 1.7, 3.6, 2.4, 2.9, 3.2]}
+teste = pd.DataFrame(data)
+
+teste.plot(x='year', y='pop')
+sns.lineplot(x="year", y="pop",
+             hue="state",
+             data=teste)
+px.line(teste, x='year', y='pop', title='Teste')
